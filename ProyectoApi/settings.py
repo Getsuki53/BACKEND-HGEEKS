24875391fd618066164def22c8878324023175d1
Getsuki53 <brian.gonzalez5353@gmail.com>
2025-07-09 11:24:07 -0400

    funciooonaaa

--- ProyectoApi/settings.py
+++ ProyectoApi/settings.py
@@ -78,6 +78,26 @@ INSTALLED_APPS = [
     'cloudinary',
 ]
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+ROOT_URLCONF = 'ProyectoApi.urls'
+
+WSGI_APPLICATION = 'ProyectoApi.wsgi.application'
+
 # Configuración de archivos estáticos
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
